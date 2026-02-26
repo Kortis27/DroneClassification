adb3689d9ac4877d98cc7bb97a45f48d9c4b0d30
Kurtis <kurtiskathol27@gmail.com>
2026-02-26 17:53:08 -0600

    payload vs harmful payload model

--- inference.py
+++ inference.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 
 model = YOLO("runs/detect/train/weights/best.pt")
 
-results = model("test2.jpg", conf=0.5)
+results = model("test4.jpg", conf=0.5)
 
 results[0].show()   # display
