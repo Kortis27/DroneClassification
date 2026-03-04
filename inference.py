75af098950034f5c96cc6bfbf024839b91c7f5a8
Kurtis <kurtiskathol27@gmail.com>
2026-03-04 10:53:02 -0600

    adjusted confidence

--- inference.py
+++ inference.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 
 model = YOLO("runs/detect/train/weights/best.pt")
 
-results = model("testimages/test6.jpg", conf=0.7)
+results = model("testimages/test5.jpg", conf=0.2)
 
 results[0].show()   # display
